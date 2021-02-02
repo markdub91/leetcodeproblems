d33e712669848716a2f5c57982af0731fc974e95
Mark Wieferich <markwieferich91@gmail.com>
2021-02-01 21:00:18 -0700

    completed validPalindrome and refactored sliding window problem

--- easy_validPalindrome.js
+++ easy_validPalindrome.js
@@ -18,5 +18,18 @@ Constraints:
 s consists only of printable ASCII characters.
 */
 var isPalindrome = function(s) {
-
+  if (s.length === 0) return true;
+  let validChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let filteredS = s.toLowerCase().split('').filter(el => validChars.indexOf(el) > -1);
+  let halfLengthIndex = Math.ceil((filteredS.length) * 0.5) - 1;
+  for (let i = 0; i <= halfLengthIndex; i++) {
+    if (filteredS[i] !== filteredS[filteredS.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(isPalindrome(''));
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('race a car'));
--- medium_longestRepeatingCharacterReplacement.js
+++ medium_longestRepeatingCharacterReplacement.js
@@ -38,41 +38,84 @@ var characterReplacement = function(s, k) {
     return s.length;
   }
 
-  let slidingWindow = (window) => {
-    let windowObj = {};
-    let windowArray = window.split('');
-    windowArray.forEach(el => {
-      if (windowObj[el]) {
-        windowObj[el]++;
+  // replace chars and check string function
+  let validCheck = (string) => {
+    let mapObj = {};
+    for (let i = 0; i < string.length; i++) {
+      if (mapObj[string[i]]) {
+        mapObj[string[i]]++;
       } else {
-        windowObj[el] = 1;
+        mapObj[string[i]] = 1;
       }
-    })
-    let windowVals = Object.values(windowObj).sort((a, b) => (a - b));
-    if (windowVals.length === 1 || (window.length - windowVals[windowVals.length - 1] <= k)) {
+    }
+    // console.log(Object.values((mapObj)));
+    let mapObjVals = Object.values(mapObj);
+    if (mapObjVals.sort((a, b) => (b - a))[0] + k >= string.length || mapObjVals.length === 1) {
       return true;
     } else {
       return false;
     }
   }
-  // define window size
-  for (let i = s.length; i >= 0; i--) {
-    // define window start index
-    for (let j = 0; j < s.length; j++) {
-      // check if start index plus window size will fit within string, else continue
-      if ((j + i) <= s.length) {
-        let testSlice = s.slice(j, j + i);
-        if (slidingWindow(testSlice)) {
-          return i;
-        }
-      } else {
-        continue;
+
+  let left = 0;
+  let right = 1;
+  let maxLength = 0;
+  // console.log(s.slice(0, 3));
+  // console.log(validCheck(s.slice(0, 3)));
+
+  while(right <= s.length) {
+    // console.log('left: ' + left + ' right: ' + right);
+    if (right <= left) {
+      right++;
+    }
+    else if (validCheck(s.slice(left, right))) {
+      if ((right - left) > maxLength) {
+        maxLength = right - left;
       }
+      right++;
+    } else {
+      left++;
     }
   }
 
+  return maxLength;
+
+  // let slidingWindow = (window) => {
+  //   let windowObj = {};
+  //   let windowArray = window.split('');
+  //   windowArray.forEach(el => {
+  //     if (windowObj[el]) {
+  //       windowObj[el]++;
+  //     } else {
+  //       windowObj[el] = 1;
+  //     }
+  //   })
+  //   let windowVals = Object.values(windowObj).sort((a, b) => (a - b));
+  //   if (windowVals.length === 1 || (window.length - windowVals[windowVals.length - 1] <= k)) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // // define window size
+  // for (let i = s.length; i >= 0; i--) {
+  //   // define window start index
+  //   for (let j = 0; j < s.length; j++) {
+  //     // check if start index plus window size will fit within string, else continue
+  //     if ((j + i) <= s.length) {
+  //       let testSlice = s.slice(j, j + i);
+  //       if (slidingWindow(testSlice)) {
+  //         return i;
+  //       }
+  //     } else {
+  //       continue;
+  //     }
+  //   }
+  // }
+
 };
 
-console.log(characterReplacement('ABAB', 2));
-console.log(characterReplacement('AABABBA', 1));
-console.log(characterReplacement('ABBB', 2));
+// console.log(characterReplacement('ABAB', 2)); // 4
+console.log(characterReplacement('AABABBA', 1)); // 4
+// console.log(characterReplacement('ABBB', 2)); // 4
+// console.log(characterReplacement('AAAA', 2)); // 4

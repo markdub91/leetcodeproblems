8a4274178f26472c5f1187a01acd1030dadb9973
Mark Wieferich <markwieferich91@gmail.com>
2021-02-16 21:38:30 -0700

    wordSearch complete, rotateImage close to complete

--- medium_rotateImage.js
+++ medium_rotateImage.js
@@ -33,5 +33,29 @@ matrix[i].length == n
 -1000 <= matrix[i][j] <= 1000
 */
 var rotate = function(matrix) {
-
+  // move groups of four around the edge, moving one level into the matrix at a time
+  let n = matrix.length;
+  let maxI = Math.floor( n / 2);
+  let maxJ = Math.ceil(n / 2);
+  for (let i = 0; i < maxI; i++) {
+    for (let j = (0 + i); j < (maxJ - i); j++) {
+      let currentTL = matrix[0 + i][0 + j];
+      let currentTR = matrix[0 + j][n - 1 - i];
+      let currentBR = matrix[n - 1 - i][n - 1 - j];
+      let currentBL = matrix[n - 1 - j][0 + i];
+      matrix[0 + i][0 + j] = currentBL;
+      matrix[0 + j][n - 1 - i] = currentTL;
+      matrix[n - 1 - i][n - 1 - j] = currentTR;
+      matrix[n - 1 - j][0 + i] = currentBR;
+    }
+  }
+  return matrix;
 };
+
+// console.log(rotate([[1,2],
+//                     [3,4]]))
+//                 // [[3,1],
+//                 //  [4,2]] expected
+// console.log(rotate([[1]])); // [[1]]
+// console.log(rotate([[1,2,3],[4,5,6],[7,8,9]])); // [[7,4,1],[8,5,2],[9,6,3]]
+console.log(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])); // [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
--- medium_wordSearch.js
+++ medium_wordSearch.js
@@ -31,5 +31,40 @@ n = board[i].length
 board and word consists only of lowercase and uppercase English letters.
 */
 var exist = function(board, word) {
-
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (horzVertChecker(board, word, i, j, {})) return true;
+    }
+  }
+  return false;
 };
+
+let horzVertChecker = function(board, word, y, x, history) {
+  if (y >= board.length || x >= board[0].length) return false;
+  // console.log(board, y, x);
+  if (word.length === 0) {
+    return true;
+  } else if (board[y][x] === word[0]) {
+    word = word.slice(1);
+    if (word.length === 0) return true;
+    history[y + " " + x] = true;
+    // above
+      // if above exists (ie if (y - 1) >= 0 and board.length >= 1)
+    if ((y > 0 && history[(y - 1) + " " + x] === undefined && horzVertChecker(board, word, y - 1, x, history)) ||
+        (y < board.length && history[(y + 1) + " " + x] === undefined && horzVertChecker(board, word, y + 1, x, history)) ||
+        (x > 0 && history[y + " " + (x - 1)] === undefined && horzVertChecker(board, word, y, x - 1, history)) ||
+        (x < board[0].length && history[y + " " + (x + 1)] === undefined && horzVertChecker(board, word, y, x + 1, history))) {
+          return true;
+    } else {
+      delete history[y + " " + x];
+    }
+  } else {
+    return false;
+  }
+}
+console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE")); // true
+console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")) // true
+console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")) // false
+console.log(exist([["C","A","A"],
+                   ["A","A","A"],
+                   ["B","C","D"]], "AAB")) // true
